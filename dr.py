e79b91c8a8eb34e34abb340dce9656bad6fcd660
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-07-15 07:14:53 -0500

    Convert main into a procedure.

--- dr.py
+++ dr.py
@@ -30,92 +30,94 @@ EMAIL_PATTERN = "[0-9A-Za-z._+-]*@[0-9A-Za-z._-]*\.[A-Za-z0-9]*"
 # | Utilities |
 # +-----------+
 
-def validORCID(orcid):
+def validORCID(volunteers, orcid):
     return orcid.lower() in volunteers
     # TO DO: Search the ORCID column, rather than use this technique
 
-def validEmail(email):
+def validEmail(volunteers, email):
     return email.lower() in volunteers
     # TO DO: Search the Email column
 
-def validName(name):
+def validName(volunteers, name):
     return name.replace(" ", "\t").lower() in volunteers
 
 # +------+-----------------------------------------------------------
 # | Main |
 # +------+
 
-# Sanity check
-if (len(sys.argv) != 4):
-    sys.exit(f"Invalid number of arguments; three required (designated reviewers file, volunteers file, file suffix). E.g.,\n  python3 {sys.argv[0]} submissions.tsv volunteers.tsv 20240717")
-
-# Grab all the info on volunteers
-volunteersFile = open(sys.argv[2])
-volunteers = volunteersFile.read().lower()
-volunteersFile.close()
-
-# Grab the indices of important volunteer columns
-volunteersFile = open(sys.argv[2])
-volunteersHeaders = volunteersFile.readline().split("\t")
-volunteersFile.close()
-VOLUNTEERS_EMAIL_COLUMN = volunteersHeaders.index("Email Address")
-VOLUNTEERS_ORCID_COLUMN = volunteersHeaders.index("ORCid")
-
-# Prepare output files
-SUFFIX = sys.argv[3]
-matched = open(f"matched{SUFFIX}.tsv", "w")
-missing = open(f"missing{SUFFIX}.tsv", "w")
-none = open(f"none{SUFFIX}.tsv", "w")
-
-matched.write("#\tTitle\tName\tEmail\tORCid\tNotes\n")
-missing.write("#\tTitle\tName\tEmail\tORCid\tNotes\n")
-none.write("#\tTitle\n")
-
-# Prepare to process the submission info
-submissionsFile = open(sys.argv[1])
-submissions = csv.reader(submissionsFile, delimiter='\t')
-
-# Grab the indices of important submission columns
-submissionsHeaders = submissions.__next__()
-SUBMISSIONS_NUMBER_COLUMN = submissionsHeaders.index("#")
-SUBMISSIONS_TITLE_COLUMN = submissionsHeaders.index("Title")
-SUBMISSIONS_DR_COLUMN = submissionsHeaders.index("Designated Reviewer")
-
-# Process all the submissions
-for entry in submissions:
-    number = entry[SUBMISSIONS_NUMBER_COLUMN]
-    title = entry[SUBMISSIONS_TITLE_COLUMN]
-    designatedReviewers = entry[SUBMISSIONS_DR_COLUMN]
-    for reviewer in designatedReviewers.split("\n"):
-        foundMatch = False
-        name = "???"
-        orcid = "???"
-        email = "???"
-        if reviewer.lower() == "none":
-            none.write(number + "\t" + title + "\n")
-        else:
-            orcids = re.findall(ORCID_PATTERN, reviewer)
-            for orcid in orcids:
-                if validORCID(orcid):
-                    foundMatch = True
-            emails = re.findall(EMAIL_PATTERN, reviewer)
-            for email in emails:
-                if validEmail(email):
-                    foundMatch = True
-            if orcid == "???" and email == "???":
-                for name in reviewer.split(","):
-                    if validName(name):
-                        foundMatch = True
-                name = reviewer
-            info = f'{number}\t{title}\t{name}\t{email}\t{orcid}\t\n'
-            if foundMatch:
-                matched.write(info)
+def __main__():
+    # Sanity check
+    if (len(sys.argv) != 4):
+        sys.exit(f"Invalid number of arguments; three required (designated reviewers file, volunteers file, file suffix). E.g.,\n  python3 {sys.argv[0]} submissions.tsv volunteers.tsv 20240717")
+    
+    # Grab all the info on volunteers
+    volunteersFile = open(sys.argv[2])
+    volunteers = volunteersFile.read().lower()
+    volunteersFile.close()
+    
+    # Grab the indices of important volunteer columns
+    volunteersFile = open(sys.argv[2])
+    volunteersHeaders = volunteersFile.readline().split("\t")
+    volunteersFile.close()
+    VOLUNTEERS_EMAIL_COLUMN = volunteersHeaders.index("Email Address")
+    VOLUNTEERS_ORCID_COLUMN = volunteersHeaders.index("ORCid")
+    
+    # Prepare output files
+    SUFFIX = sys.argv[3]
+    matched = open(f"matched{SUFFIX}.tsv", "w")
+    missing = open(f"missing{SUFFIX}.tsv", "w")
+    none = open(f"none{SUFFIX}.tsv", "w")
+    
+    matched.write("#\tTitle\tName\tEmail\tORCid\tNotes\n")
+    missing.write("#\tTitle\tName\tEmail\tORCid\tNotes\n")
+    none.write("#\tTitle\n")
+    
+    # Prepare to process the submission info
+    submissionsFile = open(sys.argv[1])
+    submissions = csv.reader(submissionsFile, delimiter='\t')
+    
+    # Grab the indices of important submission columns
+    submissionsHeaders = submissions.__next__()
+    SUBMISSIONS_NUMBER_COLUMN = submissionsHeaders.index("#")
+    SUBMISSIONS_TITLE_COLUMN = submissionsHeaders.index("Title")
+    SUBMISSIONS_DR_COLUMN = submissionsHeaders.index("Designated Reviewer")
+    
+    # Process all the submissions
+    for entry in submissions:
+        number = entry[SUBMISSIONS_NUMBER_COLUMN]
+        title = entry[SUBMISSIONS_TITLE_COLUMN]
+        designatedReviewers = entry[SUBMISSIONS_DR_COLUMN]
+        for reviewer in designatedReviewers.split("\n"):
+            foundMatch = False
+            name = "???"
+            orcid = "???"
+            email = "???"
+            if reviewer.lower() == "none":
+                none.write(number + "\t" + title + "\n")
             else:
-                missing.write(info)
+                orcids = re.findall(ORCID_PATTERN, reviewer)
+                for orcid in orcids:
+                    if validORCID(volunteers, orcid):
+                        foundMatch = True
+                emails = re.findall(EMAIL_PATTERN, reviewer)
+                for email in emails:
+                    if validEmail(volunteers, email):
+                        foundMatch = True
+                if orcid == "???" and email == "???":
+                    for name in reviewer.split(","):
+                        if validName(volunteers, name):
+                            foundMatch = True
+                    name = reviewer
+                info = f'{number}\t{title}\t{name}\t{email}\t{orcid}\t\n'
+                if foundMatch:
+                    matched.write(info)
+                else:
+                    missing.write(info)
+    
+    # Time to clean up
+    submissionsFile.close()
+    matched.close()
+    missing.close()
+    none.close()
 
-# Time to clean up
-submissionsFile.close()
-matched.close()
-missing.close()
-none.close()
-
+__main__()
